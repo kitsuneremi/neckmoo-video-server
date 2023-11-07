ed119e65a08bd2e0aee12927430c0cb7e6d03741
lily <89727355+kitsuneremi@users.noreply.github.com>
2023-11-08 03:47:40 +0700

    change api route

--- routes/livefile.js
+++ routes/livefile.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 
-router.get('/:name', async (req, res) => {
+router.get('/:link/:name', async (req, res) => {
     const name = req.params.name
     const link = req.params.link;
-    const buffer = fs.createReadStream(`D:/live/${name}`);
+    const buffer = fs.createReadStream(`D:/live/${link}/${name}`);
     buffer.pipe(res)
 })
 
